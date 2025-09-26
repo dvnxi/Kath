26f8da4de635b51aafc212b78849183dc86a1b53
Jairo Devon Daquioag <126938025+dvnxi@users.noreply.github.com>
2025-09-26 15:41:20 +0800

    Update Kath.py

--- Kath.py
+++ Kath.py
@@ -16,21 +16,21 @@ My dearest Kath,
 
 <div style='color:#FFFFFF; text-align: left; font-family: "Times New Roman", serif; font-size: 18px; line-height: 1.5;'>
 I just want to take this moment to tell you how much I love you.<br>
-Every day that passes, I realize more and more how much you mean to me.<br>
+I hope you know how much you mean to me.<br>
 You're not just my love, you're my safe place, my joy, my inspiration,<br>
 and the most beautiful blessing God has given me.<br><br>
 
-I want you to know that my patience for you will never run out (although you always worry that it might).<br>
+I want you to know that my patience for you will never run out (although you always worry that it might ehe).<br>
 No matter what comes our way, I will always stand by your side,<br>
-through your light and through your darkness.<br>
+through your light and through your darkness. Kakampi mo ako palagi.<br>
 I love all that you were, all that you are, and all that you will become (you always hear me say this).<br>
-There is nothing in this world that could change the way I see you,<br>
-that you will always be my prettiest and cutest baby girl, mwehehe.<br><br>
+There is nothing in this world that could change the way I see you.<br>
+You will always be my prettiest and cutest baby girl, mwehehe.<br><br>
 
 I'm so glad that you came into my life so unexpectedly.<br>
 You've filled my days with warmth, laughter, and meaning.<br>
 You've shown me a kind of love that feels both comforting and exciting,<br>
-like feeling home and dreaming big at the same time.<br><br>
+like feeling at home and dreaming big at the same time.<br><br>
 
 Please remember, my baby girl, that my love for you is endless.<br>
 I will hold your hand through every storm, celebrate every victory with you,<br>
